c796558f6052fb6bdcbfff3a84c41bfcafd525c4
EvanF2008 <evan118544@student-schoolsofwestfield.org>
2024-10-21 14:05:20 +0000

    erm jus started

--- script.js
+++ script.js
@@ -1,21 +1,20 @@
 /* Variable and Alert Katas */
 // These katas do not involve functions and are very basic. 
 
-/* PET KATAS */
+/* Var, Alert, Prompt, Function */
 
-/* Var Alert Prompt */
-// make a variable for pet type, prompt user for it 
-var pet_type = prompt("what pets do you have?");
-// make a variable for pet name, prompt user for it
-var pet_name = prompt("Whats the name of your pet(s)"); 
-// alert "You have a pet type named pet name 
-alert("You have a" +pet_type+ "named" +pet_name);
+/* FUNCTION main() 
 
-/* Var Alert Prompt Conditionals */
-// make a variable for pet type, prompt user for it 
-// make a variable for pet name, prompt user for it
-// alert "You have a pet type named pet name 
-// if pet is a dog, say "I like dogs, too!"
-// if it is a cat, say "I'm allergic to cats"
-// If it is not a dog or a cat, say "what an interesting pet!"
+ * Facebook wants to check if you're 13, so they ask your birth year 
+ * set age = "How old are you?"
+ * then set born = asking "What year were you born?"
+ * If myAge(age) is not equal to born, say, "Underaged", otherwise "Welcome!"
+ * @param: none (main never gets params)
+ * @return: none (main never gets returns
+ */
+function main(){
+    let age = prompt("insert age");
+    let born = prompt(" insert birth year");
+    
+
 
